b1adb2f9587f7a002c39f76c768df1a88d7f0a66
VedangJotaniya <vedangjotaniya@gmail.com>
2022-06-23 19:37:03 -0400

    commit #2 adding extract function

--- main.py
+++ main.py
@@ -1,7 +1,40 @@
-# This is a sample Python script.
+# Loading DB modules
+import pandas as pd
+import os
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def extract(src_df, start_index, end_index, selected_cols):
+    # verify id src_df is not null
+    if src_df.empty:
+        raise Exception("src_dataframe object is null.")
+
+    # verify that start and end indexes
+    if start_index < 0 or end_index < 0:
+        raise Exception("start_index or end_index is less than zero")
+    if start_index > end_index:
+        raise Exception("Index range is not proper")
+
+    print("Extracting rows= ",start_index," : ", end_index, "Total= ", end_index - start_index)
+    # extract data rows in a row object
+    data_rows = src_df.iloc[start_index:end_index, selected_cols]
+    print("data_rows in extract")
+    print(data_rows.head())
+    print("----"*20)
+    return transform(data_rows)
+
+
+def transform(rows):
+
+    # data cleaning
+    # remove invalid values
+    # Normalization
+    print(rows.head())
+    print("----" * 20)
+    return
+
+
+def load():
+    return
 
 
 def print_hi(name):
@@ -11,6 +44,15 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('Bombardier Aero')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Assuming that source csv file is in current directory
+    src_dir = "."
+    src_file_name = "patient_data.csv"
+    selected_cols = [0, 5, 6, 7, 8, 9]
+    # Create and load source csv into an object and save the object
+    print(os.path.join(src_dir, src_file_name))
+    src_df = pd.read_csv(src_file_name,
+                         encoding='ISO-8859-1')  # Assuming a standard encoding of ISO-8859-1 due to presence of UnicodeDecodeError
+
+    extract(src_df=src_df, start_index=0, end_index=2, selected_cols=selected_cols)  # range is inclusive of start and end
